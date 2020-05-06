f8d1b23d63af6420429289b5cae677264a25f0c0
Lee Netherton <ltn100@gmail.com>
2020-05-06 18:30:16 +0100

    Improvements

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@
 #include <RF24.h>
 #include <SPI.h>
 
-#define SwitchPin 8 // Arcade switch is connected to Pin 8 on NANO
-int SentMessage[1] = {0}; // Used to store value before being sent through the NRF24L01
-int RecvMessage[1] = {0};
-
 RF24 radio(9,10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
 
 const uint64_t pipes[2] = {
@@ -17,99 +13,85 @@ const uint64_t pipes[2] = {
 #define EXT_LED 2
 #define SwitchPin 3
 
-#define TX
+//#define INPUT_MODULE
+#define OUTPUT_MODULE
 
+#define LOOP_DELAY 5
 
+unsigned int communicate(unsigned int input, unsigned int timeout=0) {
+  const unsigned int delay_time = 5;
+  unsigned int output = 0x0;
 
+  radio.stopListening();
+  radio.write(&input, sizeof(input));
+  delay(delay_time);
 
-#ifdef TX
+  radio.startListening();
+  unsigned int count = 0;
+
+  while(!radio.available()) {
+    if (count*delay_time > timeout) {
+      break;
+    }
+    delay(delay_time);
+  }
+  if (radio.available()) {
+    radio.read(&output, sizeof(output));
+    radio.flush_rx();
+  }
+
+  return output;
+}
+
+#ifdef INPUT_MODULE
+///////////////////////////////////////////
+// Input:
+//  This module detects current and has
+//  a manual switch.
+///////////////////////////////////////////
 void setup(void){
   pinMode(EXT_LED, OUTPUT);
   pinMode(SwitchPin, INPUT_PULLUP);
   digitalWrite(SwitchPin,HIGH);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   radio.begin();
   radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1, pipes[1]);
   radio.setPALevel(RF24_PA_MIN);
 }
 
 void loop(void){
-  delay(100);
-  SentMessage[0] = digitalRead(SwitchPin);
-  radio.write(SentMessage, 1);
-  Serial.print("Wrote: ");
-  Serial.print(SentMessage[0]);
-  Serial.print("\n");
-  digitalWrite(EXT_LED, SentMessage[0]);
 
+  unsigned int msg = (!digitalRead(SwitchPin)) & 0x1;
+  //Serial.print("Wrote: ");
+  //Serial.print(msg);
+  //Serial.print("\n");
+  msg = communicate(msg);
+  digitalWrite(EXT_LED, msg & 0x1);
+  //Serial.print("Read: ");
+  //Serial.print(msg);
+  //Serial.print("\n");  
 }
 #else
-
+///////////////////////////////////////////
+// Output:
+//  This module switches the vaccum
+///////////////////////////////////////////
 void setup(void){
   pinMode(EXT_LED, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   radio.begin();
-  radio.openReadingPipe(1, pipe);
+  radio.openReadingPipe(1, pipes[0]);
+  radio.openWritingPipe(pipes[1]);
   radio.setPALevel(RF24_PA_MIN);
-  SentMessage[0] = 0x1;
-  radio.startListening();
 }
 
 void loop(void){
-  delay(50);
 
-  Serial.print("Waiting for radio...\n");
-  while(!radio.available()); // wait for radio
-  Serial.print("Got radio...\n");
-
-  radio.read(SentMessage, 1);
-  Serial.print("Read: ");
-  Serial.print(SentMessage[0]);
-  Serial.print("\n");
-
-  digitalWrite(EXT_LED, SentMessage[0]);
+  unsigned int msg = digitalRead(EXT_LED) & 0x1;
+  msg = communicate(msg, 500);
+  digitalWrite(EXT_LED, msg & 0x1);
 }
-
-
 #endif
-
-
-// void setup(void){
-//   pinMode(LED_BUILTIN, OUTPUT);
-//   Serial.begin(9600);
-
-//   radio.begin();
-//   radio.openWritingPipe(pipe);
-//   radio.openReadingPipe(1, pipe);
-//   radio.setPALevel(RF24_PA_MIN);
-//   radio.startListening();
-//   SentMessage[0] = 0x1;
-// }
-
-// void loop(void){
-//   delay(500);
-
-//   radio.write(SentMessage, 1);
-//   Serial.print("Wrote: ");
-//   Serial.print(SentMessage[0]);
-//   Serial.print("\n");
-
-//   delay(500);
-  
-//   Serial.print("Waiting for radio...\n");
-//   //while(!radio.available()); // wait for radio
-//   Serial.print("Got radio...\n");
-
-//   radio.read(RecvMessage, 1);
-//   Serial.print("Read: ");
-//   Serial.print(RecvMessage[0]);
-//   Serial.print("\n");
-
-//   digitalWrite(LED_BUILTIN, RecvMessage[0]);
-
-//   SentMessage[0] = 1 - RecvMessage[0];
-
-//   delay(1000);
-// }
